0a746b5925ed422a9347df0cdadf0dcf5adfce0c
Moonkey <moonkey48@icloud.com>
2023-03-28 20:02:59 +0900

    [BOJ] 10808 refactoring

--- array_algo.py
+++ array_algo.py
@@ -1,13 +1,25 @@
+# S = input()
+
+
+# arr = []
+
+# for i in range(ord('a'), ord('z') + 1):
+#     num = 0
+#     for s in S:
+#         if ord(s) == i:
+#             num += 1
+#     arr.append(num)
+
+# print(' '.join(map(str, arr)))
+
+
+# S = input()
+
+
 S = input()
+arr = [0] * 26
 
-
-arr = []
-
-for i in range(ord('a'), ord('z') + 1):
-    num = 0
-    for s in S:
-        if ord(s) == i:
-            num += 1
-    arr.append(num)
+for s in S:
+    arr[ord(s) - 97] += 1
 
 print(' '.join(map(str, arr)))
