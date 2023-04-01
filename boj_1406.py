1b910729c78f6f41de86a0312bbb4ca65bb3df47
Moonkey <moonkey48@icloud.com>
2023-04-01 13:22:26 +0900

    [BOJ] complete BOJ 1406 (linked list)

--- boj_1406.py
+++ boj_1406.py
@@ -1,85 +1,58 @@
+import sys
+input = sys.stdin.readline
 
-input  = '''abcd
-3
-P x
-L
-P y'''
-
-class ListNode:
-    def __init__(self, val):
-        self.val = val
-        self.next = None
-
-def printNodes(node:ListNode):
-    crnt_node = node
-    while crnt_node is not None:
-        print(crnt_node.val, end=' ')
-        crnt_node = crnt_node.next
-
-class SLinkedList:
+class DList:
+    class Node:
+        def __init__(self, item, prev, next):
+            self.item = item
+            self.prev = prev
+            self.next = next
+            
     def __init__(self):
-        self.head = ListNode('')
+        self.head = self.Node(None,None,None)
+        self.tail = self.Node(None,self.head, None)
+        self.head.next = self.tail
+        self.cur = self.tail
         
-    def addAtHead(self, val): #O(1)
-        node = ListNode(val)
-        node.next = self.head
-        self.head = node
-
-    #but when the list
-    def addBack(self, val): #O(n)
-        node = ListNode(val)
-        crnt_node = self.head
-        while crnt_node.next:
-            crnt_node = crnt_node.next
-        crnt_node.next = node
-
-    def findNode(self, val): #O(n)
-        crnt_node = self.head
-        while crnt_node is not None:
-            if crnt_node.val == val:
-                return crnt_node
-            crnt_node = crnt_node.next
-        raise RuntimeError('Node not found')
-
-    def addAfter(self, node, val): #O(1)
-        new_node = ListNode(val)
-        new_node.next = node.next
-        node.next = new_node
-
-    def deleteAfter(self, prev_node): #O(1)
-        if prev_node.next is not None:
-            prev_node.next = prev_node.next.next
-            
-linked_list = SLinkedList()
-
-arr = list(map(str, input.split('\n')))
-
-base_str = arr[0]
-input_length = arr[1]
-cursor = len(base_str)
-
-for i in list(base_str):
-    linked_list.addBack(i)
-    
-
-for pos in range(2,int(input_length)+2):
-    command = arr[pos].split(' ')
-    
-    if command[0] == 'L':
-        if cursor != 0:
-            cursor -= 1
-    elif command[0] == 'D':
-        if cursor != len(base_str):
-            cursor += 1
-    elif command[0] == 'B':
-        print('delete')
-    
+    def insert(self, node, item):
+        prevNode = node.prev
+        new_node = self.Node(item, prevNode, node)
+        node.prev = new_node
+        prevNode.next = new_node
         
-            
+    def delete(self, node):
+        prev_node = node.prev
+        rear_node = node. next
+        prev_node.next = rear_node
+        rear_node.prev = prev_node
         
+    def print_list(self):
+        p = self.head.next
+        while p != self.tail:
+            if p.next != self.tail:
+                print(p.item, end="")
+            else:
+                print(p.item)
+            p = p.next
 
-# L	커서를 왼쪽으로 한 칸 옮김 (커서가 문장의 맨 앞이면 무시됨)
-# D	커서를 오른쪽으로 한 칸 옮김 (커서가 문장의 맨 뒤이면 무시됨)
-# B	커서 왼쪽에 있는 문자를 삭제함 (커서가 문장의 맨 앞이면 무시됨)
-# 삭제로 인해 커서는 한 칸 왼쪽으로 이동한 것처럼 나타나지만, 실제로 커서의 오른쪽에 있던 문자는 그대로임
-# P $	$라는 문자를 커서 왼쪽에 추가함
+s = list(input().rstrip())
+dl = DList()
+
+for i in range(len(s)):
+    dl.insert(dl.tail, s[i])
+
+for i in range(int(input())):
+    o = input().split()
+    
+    if(o[0] == "L"):
+        if(dl.cur.prev.prev != None):
+            dl.cur = dl.cur.prev
+    elif(o[0] == "D"):
+        if(dl.cur.next != None):
+            dl.cur = dl.cur.next
+    elif(o[0] == "B"):
+        if(dl.cur.prev.prev != None):
+            dl.delete(dl.cur.prev)
+    else:
+        dl.insert(dl.cur, o[1])
+dl.print_list()
