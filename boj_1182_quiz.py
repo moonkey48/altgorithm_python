82e7f27ff5f4e1a08fbb5cfb786966b0f845e4eb
Moonkey <moonkey48@icloud.com>
2023-05-01 19:44:10 +0900

    [BOJ] solve boj 1182

--- boj_1182_quiz.py
+++ boj_1182_quiz.py
@@ -2,9 +2,22 @@ import sys
 input = sys.stdin.readline
 
 n, sum = list(map(int, input().rstrip().split(" ")))
-
-checked_list = [False] * n
 num_list = list(map(int, input().rstrip().split(" ")))
 
-count = 0
+count = 0    
 
+def check(cur, total):
+    global count
+    if cur == n:
+        if total == sum:
+            count += 1
+        return
+    check(cur + 1, total)
+    check(cur + 1, total + num_list[cur])
+
+
+check(0, 0)
+
+if sum == 0:
+    count -= 1
+print(count)
