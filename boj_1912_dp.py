62e7e712bf6e38f3e329372a5a97276545690c82
Moonkey <moonkey48@icloud.com>
2023-05-09 14:56:28 +0900

    [BOJ] solve boj 1912 dp

---
+++ boj_1912_dp.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+num_list = list(map(int, input().rstrip().split(" ")))
+
+dp = [0] * n
+dp[0] = num_list[0]
+
+for i in range(1, n):
+    dp[i] = max(dp[i - 1] + num_list[i], num_list[i])
+print(max(dp))
