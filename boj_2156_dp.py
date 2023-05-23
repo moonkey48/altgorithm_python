93ee8c45cefd6442c1e6034efa7862bfe8406975
Moonkey <moonkey48@icloud.com>
2023-05-23 15:09:28 +0900

    [BOJ] solve boj 2156 dp

--- boj_2156_dp.py
+++ boj_2156_dp.py
@@ -2,45 +2,47 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-wine_list = []
-dp = [0] * n
+wines = [0]
 
-for i in range(n):
-    wine_list.append(int(input()))
+for _ in range(n):
+    wines.append(int(input().rstrip()))
 
-dp[0] = wine_list[0]
-dp[1] = wine_list[0] + wine_list[1]
-dp[2] = max(wine_list[0] + wine_list[1], wine_list[0] + wine_list[2], wine_list[1] + wine_list[2])
-dp[3] = max(
-    wine_list[0] + wine_list[1] + wine_list[3],
-    wine_list[0] + wine_list[2] + wine_list[3],
-    wine_list[1] + wine_list[2]
-    )
-for i in range(3, n):
-    dp[i] = max(
-        dp[i - 3] + wine_list[i - 1] + wine_list[i],
-        dp[i - 2] + wine_list[i]
-    )
-# print(wine_list)
-# print(dp)
-print(max(dp))
+dp = [0] * (n + 1)
 
-# D[i]는 i개의 포도주가 있을때 마실 수 있는 포도주의 최대 양
-# D[0] = wine_list[0]
-# D[1] = wine_list[0] + wine_list[1]
-# D[2] = max(wine_list[0] + wine_list[1], wine_list[1] + wine_list[2], wine_list[0] + wine_list[2])
-# D[3] = 
-#
-# 6
-# 6 + 10
-# 6 + 13 / 6 + 10 / 10 + 13 
-# 6 + 10 + 9 / 6 + 13 + 9
-# 6 + 10 + 9 + 8 / 6 + 10 + 9 + 8 / 6 + 13 + 8 / 10 + 13 + 8 /
-dp[i - 3] + wine_list[i - 1] + wine_list[i]
-dp[i - 2] + wine_list[i]
+if n == 1:
+    print(wines[1])
+elif n == 2:
+    print(wines[1] + wines[2])
+else:
+    dp[1] = wines[1]
+    dp[2] = wines[1] + wines[2]
+    for i in range(3, n + 1):
+        dp[i] = max(
+            dp[i - 2] + wines[i],
+            dp[i - 3] + wines[i - 1] + wines[i],
+        )
+        dp[i] = max(dp[i -1], dp[i])
+    print(dp[n])
 
 
+# 10 
+# 977 
+# 200 
+# 517 
+# 851 
+# 23 
+# 662 
+# 880 
+# 815 
+# 26 
+# 214
 
 
-
-# D[i] = max(D[i - 3] + wine_list[i - 1] + wine_list[i],)
+# 7 
+# 1 
+# 10 
+# 1 
+# 10 
+# 1
+# 10
+# 1
