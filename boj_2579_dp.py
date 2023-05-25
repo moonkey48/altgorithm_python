6bad7e8501bff00c657af882cc2c60ed23f61668
Moonkey <moonkey48@icloud.com>
2023-05-25 19:57:58 +0900

    [BOJ] solve boj 2579

--- boj_2579_dp.py
+++ boj_2579_dp.py
@@ -1,27 +1,42 @@
 import sys
+
 input = sys.stdin.readline
 
 n = int(input())
-arr = []
+arr = [0]
+dp = [0] * (n + 2)
+
 for i in range(n):
-    arr.append(int(input()))
+    arr.append(int(input().rstrip()))
 
-dp = [0] * n
-pos = n - 1
-dp[pos] = arr[n - 1]
+if n == 1:
+    print(arr[1])
+elif n == 2:
+    print(arr[1] + arr[2])
+else:
+    dp[1] = arr[1]
+    dp[2] = arr[1] + arr[2]
 
-while pos >= 0:
-    if pos == 1:
-        dp[0] += dp[pos]
-        pos -= 1
-        break
-    if arr[pos - 1] < arr[pos - 2]:
-        dp[pos - 2] = arr[pos - 2] + dp[pos]
-        pos -= 2
-    else:
-        dp[pos - 1] = arr[pos - 1] + dp[pos]
-        pos -= 1
-        
-print(dp)
-    
-    
+    for i in range(3, n + 1):
+        dp[i] = max(
+            dp[i - 3] + arr[i - 1] + arr[i],
+            dp[i - 2] + arr[i]
+        )            
+                
+        # dp[i] = max(dp[i - 1], dp[i])
+    print(dp)
+    print(dp[n])
+
+# 4
+# 1
+# 4
+# 2
+# 3
+
+# 6
+# 123
+# 14
+# 3
+# 2
+# 13
+# 4
