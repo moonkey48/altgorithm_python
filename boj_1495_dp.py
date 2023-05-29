e4ff22ad0d5cf12192fe4d057da5fbeb1722b979
Moonkey <moonkey48@icloud.com>
2023-05-29 13:28:27 +0900

    [BOJ] solve boj 1485 dp

--- boj_1495_dp.py
+++ boj_1495_dp.py
@@ -1,12 +1,33 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-n, cur, v_max = list(map(int, input().rstrip().split(" ")))
-v_list = list(map(int, input().rstrip().split(" ")))
+n, start_volume, max_volume = list(map(int, input().rstrip().split(" ")))
+volume_list = list(map(int, input().rstrip().split(" ")))
 
-dp = [[0,0] for _ in range(n)]
- 
-for i in range(n):
-    if dp[i][0] - v_list[i] >= 0:    
-        dp[i][0] = dp[i][0] - v_list[i]
-    elif dp[i]
+
+before = deque([start_volume])
+
+for i in range(len(volume_list)):
+    now = deque([])
+    for j in range(len(before)):
+        before_value = before.popleft()
+        if before_value + volume_list[i] <= max_volume:
+            if before_value + volume_list[i] not in now:
+                now.append(before_value + volume_list[i]) 
+        if before_value - volume_list[i] >= 0:
+            if before_value - volume_list[i] not in now:
+                now.append(before_value - volume_list[i])
+    before = now
+    if len(before) == 0:
+        break 
+    #  3 1 // 
+
+if len(before) == 0:
+    print(-1)
+else:
+    print(max(before))
+
+
+
+
